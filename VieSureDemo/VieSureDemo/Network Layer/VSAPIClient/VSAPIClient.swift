a46a0a6218f6fd8da280e8df0b851111ce97d1cf
Stanislav Kimov <stanislav.kimov@bloomdiagnostics.com>
2022-11-24 14:30:57 +0100

    Small fix for APIClient

--- VieSureDemo/VieSureDemo/Network Layer/VSAPIClient/VSAPIClient.swift
+++ VieSureDemo/VieSureDemo/Network Layer/VSAPIClient/VSAPIClient.swift
@@ -17,13 +17,18 @@ class VSAPIClient: APIClient, IVSAPI {
             static let articlesList = "/de42e6d9-2d03-40e2-a426-8953c7c94fb8"
         }
     }
-
-    /// Load articles list - GET 'https://run.mocky.io/v3/de42e6d9-2d03-40e2-a426-8953c7c94fb8'
-     func articlesList() -> AnyPublisher<[APIModel.Response.Article], VSError> {
-        let urlString = "\(VSAPIClient.URLs.host)\(VSAPIClient.URLs.apiVersion)\(VSAPIClient.URLs.Endpoints.articlesList)"
+    
+    private func makeUrl(host: String, apiVersion: String, endpoint: String) -> URL {
+        let urlString = "\(host)\(apiVersion)\(endpoint)"
         guard let url = URL(string: urlString) else {
             unexpectedCodePath(message: "Invalid URL.")
         }
+        return url
+    }
+
+    /// Load articles list - GET 'https://run.mocky.io/v3/de42e6d9-2d03-40e2-a426-8953c7c94fb8'
+     func articlesList() -> AnyPublisher<[APIModel.Response.Article], VSError> {
+        let url = makeUrl(host: VSAPIClient.URLs.host, apiVersion: VSAPIClient.URLs.apiVersion, endpoint: VSAPIClient.URLs.Endpoints.articlesList)
         guard let request = URLComponents(url: url, resolvingAgainstBaseURL: true)?
             .request else {
             unexpectedCodePath(message: "Failed to create request.")
