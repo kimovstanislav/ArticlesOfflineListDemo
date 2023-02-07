88d8a5c986b44cd2a36ab9239e64c9f530af875d
skimov <kimovstanislav@gmail.com>
2023-02-07 20:22:39 +0100

    Added pods

---
+++ VieSureDemo/Pods/Target Support Files/Pods-ArticlesOfflineListDemo-ArticlesOfflineListDemoUITests/Pods-ArticlesOfflineListDemo-ArticlesOfflineListDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ArticlesOfflineListDemo_ArticlesOfflineListDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ArticlesOfflineListDemo_ArticlesOfflineListDemoUITests
+@end
